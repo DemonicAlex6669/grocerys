abf7cd4f0d7d13bdcd5fca6e655d8595d3036f7b
alex watson <alexwatsondemon@gmail.com>
2024-05-12 15:45:24 -0700

    skelleton 0.5

--- grocerys.c
+++ grocerys.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 
-void main (argc, argv[])
+int main (void)
 {
+arr[] = {};
 
+	if (arr != '/0')
+	{
+		char y/n;
+
+		toupper(y/n);
+
+		getf(y/n);
+
+			if (strcmp(y/n, 'Y') == 0)
+			{
+			.
+			}
+			else if (strcmp(y/n, 'N') == 0)
+			{
+			.
+			}
+	}
 }
