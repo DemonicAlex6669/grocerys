71223ab60c3246268d0199d1f250b8fa8439770f
alex watson <alexwatsondemon@gmail.com>
2024-05-13 15:34:38 -0700

    skelleton 0.6

--- grocerys.c
+++ grocerys.c
@@ -12,13 +12,25 @@ arr[] = {};
 
 		getf(y/n);
 
+		file *fpt = fopen("groceries.txt", "a");
+			if (fpt == NULL)
+			{
+			printf("failed to make file");
+			return 1;
+			}
+
 			if (strcmp(y/n, 'Y') == 0)
 			{
-			.
+			int i = 0;
+
+			fprint(fpt, "%s", arr[i]);
+
+			i++;
 			}
 			else if (strcmp(y/n, 'N') == 0)
 			{
-			.
+			i++;
 			}
 	}
+fclose(ftp);
 }
