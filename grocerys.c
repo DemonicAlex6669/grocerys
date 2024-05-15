2c338136203282dc81c4e3a4098e76a9c4dfc7f9
alex watson <alexwatsondemon@gmail.com>
2024-05-15 15:41:40 -0700

    0.9
    almost works

--- grocerys.c
+++ grocerys.c
@@ -1,33 +1,32 @@
 #include <stdio.h>
+#include <ctype.h>
+#include <unistd.h>
 
 int main (void)
 {
-arr[] = {};
+char *arr[] = {"orange", "banana", "cantalope", "lettus", "green onion", "yellow onion", "potato", "carrot", "cutcumber", "ground beef", "chicken", "beef", "steak", "fresh burger", "50\% deli", "hotdog", "sasage", "ham", "biscut", "egg", "sour cream", "cottage chease", "orange juice", "butter", "cheese", "fake cheese", "frozen chicken", "frozen burger", "yogert", "frozen meal", "frozen sasage", "hashbrown patty", "hash brown", "ice cream", "creamer", "chip", "cookie", "pancake", "surup", "flower", "sugar", "penut butter", "jelly", "bread", "hotdog bun", "burger bun", "outdoor bun", "english muffin", "pasta", "pasta sauce", "tuna", "mac", "chilli", "beans", "pickles", "olive", "ketchupt", "mustard", "bbq", "ranch", "papertowel", "toilet paper", "dawn", "paper plate", "body soap", "shampoo"};
 int arrleng = sizeof arr/ sizeof arr[0];
 
-	if (arr != '/0')
-	{
-		char y/n;
+	char yn;
 
-		toupper(y/n);
+	FILE *fpt = fopen("groceries.txt", "a");
+		if (fpt == NULL)
+		{
+		printf("failed to make file");
+		return 1;
+		}
+		for (int i = 0; i < arrleng; i++)
+		{
+		printf("%s", arr[i]);
 
-		getf(y/n);
 
-		file *fpt = fopen("groceries.txt", "a");
-			if (fpt == NULL)
+		scanf("%c", &yn);
+
+		toupper(yn);
+			if (strcmp(&yn, "Y") == 0)
 			{
-			printf("failed to make file");
-			return 1;
+			fprintf(fpt, "%s \n", arr[i]);
 			}
-			for (i = 0, i < arrleng, i++)
-			{
-				if (strcmp(y/n, 'Y') == 0)
-				{
-				int i = 0;
-
-				fprint(fpt, "%s", arr[i]);
-				}
-			}
-	}
-fclose(ftp);
+		}
+	fclose(fpt);
 }
