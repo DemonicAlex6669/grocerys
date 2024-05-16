2967b98ba91996d37618544ffa0c16368c2a40a0
alex watson <alexwatsondemon@gmail.com>
2024-05-16 09:09:18 -0700

    version 1.1
    minor change to list

--- grocerys.c
+++ grocerys.c
@@ -4,7 +4,7 @@
 
 int main (void)
 {
-char *arr[66] = {"orange", "banana", "cantalope", "lettus", "green onion", "yellow onion", "potato", "carrot", "cutcumber", "ground beef", "chicken", "beef", "steak", "fresh burger", "50\% deli", "hotdog", "sasage", "ham", "biscut", "egg", "sour cream", "cottage chease", "orange juice", "butter", "cheese", "fake cheese", "frozen chicken", "frozen burger", "yogert", "frozen meal", "frozen sasage", "hashbrown patty", "hash brown", "ice cream", "creamer", "chip", "cookie", "pancake", "surup", "flower", "sugar", "penut butter", "jelly", "bread", "hotdog bun", "burger bun", "outdoor bun", "english muffin", "pasta", "pasta sauce", "tuna", "mac", "chilli", "beans", "pickles", "olive", "ketchupt", "mustard", "bbq", "ranch", "papertowel", "toilet paper", "dawn", "paper plate", "body soap", "shampoo"};
+char *arr[68] = {"orange", "banana", "cantalope", "lettus", "green onion", "yellow onion", "potato", "carrot", "cutcumber", "ground beef", "chicken", "beef", "steak", "fresh burger", "50\% deli", "hotdog", "sasage", "sasage patty", "bacon", "ham", "biscut", "egg", "sour cream", "cottage chease", "orange juice", "butter", "cheese", "fake cheese", "frozen chicken", "frozen burger", "yogert", "frozen meal", "frozen sasage", "hashbrown patty", "hash brown", "ice cream", "creamer", "chip", "cookie", "pancake", "surup", "flower", "sugar", "penut butter", "jelly", "bread", "hotdog bun", "burger bun", "outdoor bun", "english muffin", "pasta", "pasta sauce", "tuna", "mac", "chilli", "beans", "pickles", "olive", "ketchupt", "mustard", "bbq", "ranch", "papertowel", "toilet paper", "dawn", "paper plate", "body soap", "shampoo"};
 int arrleng = sizeof arr/ sizeof arr[0];
 
 
